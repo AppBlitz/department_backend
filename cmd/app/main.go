69031250af87b3f7dc5c89d8f6ae85f3adbde379
Carlos Fabian Corrales Zapata <fabianzapata98@outlook.com>
2026-02-21 18:52:08 -0500

    structure

---
+++ cmd/app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/AppBlitz/department_backend/internal/app"
+	"github.com/AppBlitz/department_backend/internal/database/mysqls"
+)
+
+func main() {
+	db, err := mysqls.ConnectionDatabaseMysql()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		// fmt.Print("Success conexion")
+		defer func() {
+			err := db.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}()
+		app.Run(db)
+	}
+}
